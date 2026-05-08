666cae085eb0083c881e1508a167c0ce33c9a07b
Laura  Rozo <admin@promoflow.net>
2026-05-08 12:47:58 -0500

    “…”

--- global/navbar/navbar.php
+++ global/navbar/navbar.php
@@ -7,58 +7,38 @@ $jsFile  = $base . '/global/navbar/navbar.js';
 $cssVer = is_file($cssFile) ? filemtime($cssFile) : '';
 $jsVer  = is_file($jsFile) ? filemtime($jsFile) : '';
 ?>
-<link rel="stylesheet" href="global/navbar/navbar.css<?= $cssVer ? '?v=' . $cssVer : '' ?>">
+
+<link rel="stylesheet" href="/global/navbar/navbar.css<?= $cssVer ? '?v=' . $cssVer : '' ?>">
+
 <nav class="nav">
-  <a href="https://dlarcs.com" class="nav__logo">
-      <img class="img_logo"src="global/navbar/img/logo_blanco.png" alt="">
-      <h2 class="name_logo">D'LARCS</h2>
+  <a href="/" class="nav__logo">
+    <img class="img_logo" src="/global/navbar/img/logo_blanco.png" alt="D'LARCS">
+    <h2 class="name_logo">D'LARCS</h2>
   </a>
 
   <div class="nav__links">
-    <a href="#" class="nav__link">
-      <span class="nav__text">Inicio</span>
-      <span class="nav__text nav__text--hover">Inicio</span>
-    </a>
-
-    <a href="#" class="nav__link">
-      <span class="nav__text">Productos</span>
-      <span class="nav__text nav__text--hover">Productos</span>
-    </a>
-
-    <a href="portfolio/index.php" class="nav__link">
-      <span class="nav__text">Portafolio</span>
-      <span class="nav__text nav__text--hover">Portafolio</span>
-    </a>
-
-    <a href="#" class="nav__link">
-      <span class="nav__text">Proyectos</span>
-      <span class="nav__text nav__text--hover">Proyectos</span>
-    </a>
+    <a href="/" class="nav__link"><span>Inicio</span></a>
+    <a href="#" class="nav__link"><span>Productos</span></a>
+    <a href="/portfolio/index.php" class="nav__link"><span>Portafolio</span></a>
+    <a href="#" class="nav__link"><span>Proyectos</span></a>
   </div>
 
   <div class="nav__actions">
-    <button class="nav__btn nav__btn--line">Contactanos</button>
-    <!-- <button class="nav__btn nav__btn-fill">Get Started</button> -->
+    <button class="nav__btn nav__btn--line">Contáctanos</button>
   </div>
 
   <button id="navToggle" class="nav__toggle" aria-label="Open menu" aria-expanded="false">
-  <svg class="nav__icon nav__icon--menu" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-    <path d="M4 6h16M4 12h16M4 18h16" />
-  </svg>
-
-  <svg class="nav__icon nav__icon--close" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-    <path d="M6 6l12 12M18 6L6 18" />
-  </svg>
-</button>
+    ☰
+  </button>
 
   <div id="navMenu" class="nav__mobile">
-    <a class="nav__mobile-link" href="#">Inicio</a>
+    <a class="nav__mobile-link" href="/">Inicio</a>
     <a class="nav__mobile-link" href="#">Productos</a>
-    <a class="nav__mobile-link" href="../portfolio/index.php">Portafolio</a>
-    <a class="nav__mobile-link" href="#">Estilos</a>
+    <a class="nav__mobile-link" href="/portfolio/index.php">Portafolio</a>
+    <a class="nav__mobile-link" href="#">Proyectos</a>
 
-    <button class="nav__btn nav__btn--line">Contact</button>
-    <!-- <button class="nav__btn nav__btn-fill">Get Started</button> -->
+    <button class="nav__btn nav__btn--line">Contáctanos</button>
   </div>
 </nav>
-<script defer src="global/navbar/navbar.js<?= $jsVer ? '?v=' . $jsVer : '' ?>"></script>
+
+<script defer src="/global/navbar/navbar.js<?= $jsVer ? '?v=' . $jsVer : '' ?>"></script>
