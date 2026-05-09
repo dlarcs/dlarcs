a0fdc7f17a1cd2466e59c3353a749683f09b18fb
Laura  Rozo <admin@promoflow.net>
2026-05-09 14:37:35 -0500

    “…”

--- global/navbar/navbar.js
+++ global/navbar/navbar.js
@@ -1,22 +1,23 @@
-const navToggle = document.getElementById("navToggle");
-const navMenu = document.getElementById("navMenu");
+const navToggle=document.getElementById("navToggle");
+const navMenu=document.getElementById("navMenu");
+navToggle.addEventListener("click", (event)=> {
+	event.stopPropagation();
+	navMenu.classList.toggle("is-open");
+	navToggle.classList.toggle("is-open");
+	const isOpen=navMenu.classList.contains("is-open");
+	navToggle.setAttribute("aria-expanded", isOpen);
+}
 
-navToggle.addEventListener("click", (event) => {
-  event.stopPropagation();
+);
+navMenu.addEventListener("click", (event)=> {
+	event.stopPropagation();
+}
 
-  navMenu.classList.toggle("is-open");
-  navToggle.classList.toggle("is-open");
+);
+document.addEventListener("click", ()=> {
+	navMenu.classList.remove("is-open");
+	navToggle.classList.remove("is-open");
+	navToggle.setAttribute("aria-expanded", "false");
+}
 
-  const isOpen = navMenu.classList.contains("is-open");
-  navToggle.setAttribute("aria-expanded", isOpen);
-});
-
-navMenu.addEventListener("click", (event) => {
-  event.stopPropagation();
-});
-
-document.addEventListener("click", () => {
-  navMenu.classList.remove("is-open");
-  navToggle.classList.remove("is-open");
-  navToggle.setAttribute("aria-expanded", "false");
-});
+);
